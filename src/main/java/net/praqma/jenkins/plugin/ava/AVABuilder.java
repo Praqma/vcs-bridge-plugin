bee225c6cd571084255e84da29b0cb02dec90176
wolfgarnet <wolfgarnet@gmail.com>
2011-10-28 11:53:22 +0200

    Added abstact configurations

--- src/main/java/net/praqma/jenkins/plugin/ava/AVABuilder.java
+++ src/main/java/net/praqma/jenkins/plugin/ava/AVABuilder.java
@@ -9,6 +9,8 @@ import net.praqma.vcs.model.exceptions.ElementDoesNotExistException;
 import net.praqma.vcs.model.exceptions.ElementNotCreatedException;
 import net.praqma.vcs.util.ClearCaseUCM;
 import net.praqma.vcs.util.configuration.AbstractConfiguration;
+import net.praqma.vcs.util.configuration.exception.ConfigurationException;
+import net.praqma.vcs.util.configuration.implementation.ClearCaseConfiguration;
 import net.praqma.vcs.util.configuration.implementation.MercurialConfiguration;
 import net.sf.json.JSONException;
 import net.sf.json.JSONObject;
@@ -30,55 +32,43 @@ public class AVABuilder extends Builder {
 
 	private AbstractConfiguration source;
 	private AbstractConfiguration target;
-	
+
 	private boolean processingAll;
-	
-	private File sourcePath;
-	private File targetPath;
-	
-	private String sourceBranch;
-	private String targetBranch;
-	
+
 	private static StreamAppender app = new StreamAppender( System.out );
-	
+
 	static {
-        app.setTemplate( "[%level]%space %message%newline" );
-        net.praqma.util.debug.Logger.addAppender( app );
-       	app.setMinimumLevel( net.praqma.util.debug.Logger.LogLevel.DEBUG );
+		app.setTemplate( "[%level]%space %message%newline" );
+		net.praqma.util.debug.Logger.addAppender( app );
+		app.setMinimumLevel( net.praqma.util.debug.Logger.LogLevel.DEBUG );
 	}
 
 	@DataBoundConstructor
-	public AVABuilder( /* Source */ String sourcePath, String sourceBranch, 
-			           /* Target */ String targetPath, String targetBranch, 
-			           /* Common */ boolean processingAll ) {
-		
+	public AVABuilder( AbstractConfiguration source, AbstractConfiguration target, boolean processingAll ) {
+
 		logger.warning( "Constructing" );
-		
+
 		this.processingAll = processingAll;
-		
-		/* Source */
-		this.sourcePath = new File( sourcePath );
-		this.sourceBranch = sourceBranch;
-		
-		/* Target */
-		this.targetPath = new File( targetPath );
-		this.targetBranch = targetBranch;
+
+		this.source = source;
+		this.target = target;
 	}
 
 	public boolean perform( AbstractBuild<?, ?> build, Launcher launcher, BuildListener listener ) throws InterruptedException {
 		PrintStream out = listener.getLogger();
-		
-		out.println( "[AVA] Setting up source, " + this.sourcePath  );
-		
-		out.println( "[AVA] Setting up target, " + this.targetPath  );
-		
+
+		out.println( "[AVA] Setting up source, " + this.source.getPathName() );
+
+		out.println( "[AVA] Setting up target, " + this.target.getPathName() );
+
 		return true;
 	}
-	
+
 	/**** Getters for Jenkins UI ****/
-	
+
 	/**
 	 * Get the path of the branch
+	 * 
 	 * @param st
 	 * @return
 	 */
@@ -86,18 +76,19 @@ public class AVABuilder extends Builder {
 		try {
 			if( st.startsWith( "s" ) ) {
 				/* Source */
-				return this.sourcePath.toString();
+				return this.source.getPathName();
 			} else {
 				/* Target */
-				return this.targetPath.toString();
+				return this.target.getPathName();
 			}
-		} catch( Exception e ) {
+		} catch (Exception e) {
 			return null;
 		}
 	}
-	
+
 	/**
 	 * Get the branch name
+	 * 
 	 * @param st
 	 * @return
 	 * @throws ElementNotCreatedException
@@ -106,31 +97,41 @@ public class AVABuilder extends Builder {
 	public String getBranch( String st ) throws ElementNotCreatedException, ElementDoesNotExistException {
 		if( st.startsWith( "s" ) ) {
 			/* Source */
-			return this.sourceBranch;
+			if( source instanceof MercurialConfiguration ) {
+				return ( (MercurialConfiguration) this.source ).getBranchName();
+			}
 		} else {
 			/* Target */
-			return this.targetBranch;
+			if( target instanceof MercurialConfiguration ) {
+				return ( (MercurialConfiguration) this.target ).getBranchName();
+			}
 		}
+		return "";
 	}
-	
+
 	public boolean isProcessingAll() {
 		return processingAll;
 	}
-	
+
 	public boolean isOfType( String st, String type ) {
 		logger.fine( "ST=" + st + ". TYPE:" + type );
 		if( st.startsWith( "s" ) ) {
 			/* Source */
 			if( type.equals( "hg" ) && source instanceof MercurialConfiguration ) {
 				return true;
+			} else if( type.equals( "ccfp" ) && source instanceof ClearCaseConfiguration ) {
+				return true;
 			}
+			
 		} else {
 			/* Target */
-			if( type.equals( "hg" ) && source instanceof MercurialConfiguration ) {
+			if( type.equals( "hg" ) && target instanceof MercurialConfiguration ) {
+				return true;
+			} else if( type.equals( "ccfp" ) && target instanceof ClearCaseConfiguration ) {
 				return true;
 			}
 		}
-		
+
 		return false;
 	}
 
@@ -144,74 +145,51 @@ public class AVABuilder extends Builder {
 		public String getDisplayName() {
 			return "AVA Bridging";
 		}
-		
+
 		private AbstractConfiguration getConfiguration( JSONObject data ) {
 			AbstractConfiguration config = null;
-			
+
 			String type = data.getString( "value" );
-			
+
 			File path = new File( data.getString( "viewpath" ) );
-			
+			String pathName = data.getString( "viewpath" );
+
+			/* ClearCase */
 			if( type.equals( "ccfp" ) ) {
 				try {
-					config = ClearCaseUCM.getConfigurationFromView( path );
-				} catch( Exception e ) {
-					logger.severe( "Unable to make CC view: " + e.getMessage() );
+					config = new ClearCaseConfiguration( pathName, null, null, null, null, null );
+				} catch (ConfigurationException e) {
+					logger.severe( "Could not create ClearCase configuration" );
 					e.printStackTrace();
 				}
+
+				/* Mercurial */
 			} else if( type.equals( "hg" ) ) {
 				String branch = data.getString( "branch" );
-				config = new MercurialConfiguration( path, branch );
+				config = new MercurialConfiguration( pathName, branch );
 			}
-			
+
 			return config;
 		}
 
 		@Override
 		public Builder newInstance( StaplerRequest req, JSONObject data ) {
 			System.out.println( data.toString( 2 ) );
-			
-			JSONObject source = data.getJSONObject( "source_branchType" );
-			JSONObject target = data.getJSONObject( "target_branchType" );
-			
-			/* Source */
-			String sview = null;
-			try {
-				sview = source.getString( "viewpath" );
-			} catch( JSONException e ) {
-				sview = "source";
-			}
-			
-			String sbranch = null;
-			try {
-				sbranch = source.getString( "branch" );
-			} catch( JSONException e ) {
-				
-			}
-			
-			/* Target */
-			String tview = null;
-			try {
-				tview = target.getString( "viewpath" );
-			} catch( JSONException e ) {
-				tview = "target";
-			}
-			
-			String tbranch = null;
-			try {
-				tbranch = target.getString( "branch" );
-			} catch( JSONException e ) {
-				
-			}
-			
+
+			JSONObject sourceData = data.getJSONObject( "source_branchType" );
+			JSONObject targetData = data.getJSONObject( "target_branchType" );
+
+			AbstractConfiguration source = getConfiguration( sourceData );
+			AbstractConfiguration target = getConfiguration( targetData );
+
 			boolean all = true;
 			try {
 				all = data.getBoolean( "processingAll" );
-			} catch( JSONException e ) {
-				
+			} catch (JSONException e) {
+
 			}
-			
-			return new AVABuilder( sview, sbranch, tview, tbranch, all );
+
+			return new AVABuilder( source, target, all );
 		}
 
 		@Override
